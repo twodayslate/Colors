8a41cb65ec2c6a1e91a7cb0d6126263da7a170b5
twodayslate <zac@gorak.us>
2019-10-28 22:30:34 -0400

    Initial Commit

---
+++ Colors/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Colors
+//
+//  Created by Zachary Gorak on 10/28/19.
+//  Copyright © 2019 twodayslate. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Colors/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Colors
+//
+//  Created by Zachary Gorak on 10/28/19.
+//  Copyright © 2019 twodayslate. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
